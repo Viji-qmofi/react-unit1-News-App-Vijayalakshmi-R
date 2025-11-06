564ba33bc361c64b1bfdef4dfd29395b98b9ccc7
Viji Ramu <vijiramu@gmail.com>
2025-11-06 10:21:48 -0600

    html1

--- react-news-app/src/App.jsx
+++ react-news-app/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import News from './components/News'
 
 function App() {
  
   return (
-    <>
-      
-    </>
+    <div className="container">
+      <div className="news-app">
+        <News />
+      </div>
+    </div>
   )
 }
 
