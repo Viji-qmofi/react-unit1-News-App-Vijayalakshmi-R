07a55628918c5c911e2c7a08bd9b22ed4461d3ad
Viji Ramu <vijiramu@gmail.com>
2025-11-14 18:53:41 -0600

    Functional News Modal

--- react-news-app/src/components/NewsModal.jsx
+++ react-news-app/src/components/NewsModal.jsx
@@ -2,26 +2,53 @@ import React from 'react'
 import './NewsModal.css'
 import demoImg from '../assets/images/demo.jpg'
 
-const NewsModal = () => {
-  return (
-    <div className="modal-overlay">
-      <div className="modal-content">
-            <span className="close-button">
-                  <i className="fa-solid fa-xmark"></i>
-            </span>
-            <img src={demoImg} alt="Modal Image" className="modal-image" />
-            <h2 className="modal-title">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, quos.
-            </h2>
-            <p className="modal-source">Source: The Gaurdian</p>
-            <p className="modal-date">Nov 15, 2025, 04.15pm</p>
-            <p className="modal-content-text">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut harum quidem exercitationem eligendi nihil, dolore amet nobis! Dolor quia minus consectetur provident dolorem vero magnam ipsa adipisci, quasi doloremque iusto laborum perspiciatis aspernatur debitis voluptas.
-            </p>
-            <a href="#" className="read-more-link">Read More</a> 
-      </div>
-    </div>
-  )
+const NewsModal = ({ show, article, onClose }) => {
+      if (!show) {
+            return null;
+      }
+      return (
+            <div className="modal-overlay">
+                  <div className="modal-content">
+                        <span className="close-button" onClick={onClose}>
+                              <i className="fa-solid fa-xmark"></i>
+                        </span>
+                        {article && (
+                              <>
+                                    <img src={article.image} alt={article.title} className="modal-image" />
+                                    <h2 className="modal-title">
+                                          {article.title}
+                                    </h2>
+                                    <p className="modal-source">
+                                          Source: {article.source.name}
+                                    </p>
+                                    <p className="modal-date">
+                                          {new Date(article.publishedAt).
+                                                toLocaleString('en-us', {
+                                                      month: 'short',
+                                                      day: '2-digit',
+                                                      year: 'numeric',
+                                                      hour: '2-digit',
+                                                      minute: '2-digit',
+
+                                                }
+                                          )}
+                                    </p>
+                                    <p className="modal-content-text">
+                                          {article.content}
+                                    </p>
+                                    <a
+                                          href={article.url}
+                                          target="_blank"
+                                          rel="noopener noreferrer"
+                                          className="read-more-link"
+                                    >
+                                          Read More
+                                    </a>
+                              </>
+                        )}
+                  </div>
+            </div>
+      )
 }
 
 export default NewsModal
