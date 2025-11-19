958a16a165b06ac517842a8c21f4c3d006663258
Viji Ramu <vijiramu@gmail.com>
2025-11-18 21:43:57 -0600

    Custom Validation

--- react-news-app/src/components/Common/Footer.jsx
+++ react-news-app/src/components/Common/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Footer.css'; // Optional: separate CSS
+import './Footer.css'; 
 
 const Footer = () => {
   return (
--- react-news-app/src/components/Pages/News/Modals/Modal.jsx
+++ react-news-app/src/components/Pages/News/Modals/Modal.jsx
@@ -1,29 +1,19 @@
-{/* Reusable Component -> Used in News and BookMarks Features */}
+{/* Reusable Component -> Used in News and BookMarks Features */ }
 import React from 'react';
 import './Modal.css';
 import './NewsModal.css'
 
 const Modal = ({ show, onClose, children }) => {
+  
   if (!show) return null;
 
   return (
     <div className="modal-overlay">
-
-      {/* 
-      Prevents clicks inside the modal from closing it.
-      The backdrop listens for clicks to close the modal, 
-      so we stop the event here to avoid bubbling.
-    */}
-       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-
-        {/* CLOSE BUTTON */}
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <span className="close-button" onClick={onClose}>
           <i className="fa-solid fa-xmark"></i>
         </span>
-
-        {/* Render anything passed inside the modal */}
         {children}
-        
       </div>
     </div>
   );
